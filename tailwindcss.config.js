ddb94f761720f3dba22cb6a5e4b87f7c2f263991
atctex <frostmind@yandex.ru>
2025-05-09 10:11:15 +0200

    feat: Style submission page

--- tailwindcss.config.js
+++ tailwindcss.config.js
@@ -8,7 +8,7 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    "@tailwindcss/forms",
-    "@tailwindcss/typography",
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
   ],
 }
