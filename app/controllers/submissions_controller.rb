b00829b6e741cca95107c3b9df2cca0798ef0411
atctex <frostmind@yandex.ru>
2025-05-10 13:12:59 +0200

    feat: Show user feed on index page

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -5,7 +5,13 @@ class SubmissionsController < ApplicationController
 
   # GET /submissions or /submissions.json
   def index
-    @submissions = Submission.all
+    if user_signed_in?
+      @feed_title = 'My feed'
+      @submissions = current_user.subscribed_submissions
+    else
+      @feed_title = 'Select a community'
+      @submissions = Submission.all
+    end
   end
 
   # GET /submissions/1 or /submissions/1.json
